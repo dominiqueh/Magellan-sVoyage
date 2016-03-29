72d63a1528a2fc800dabbe50a1460d4e3409fc6a
Student <Student@Mac-mini-3.local>
2016-03-29 15:36:54 -0600

    got routes to work

--- server.js
+++ server.js
@@ -1,26 +1,64 @@
+// =+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+=
+// Requires
+// =+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+=
 
-// Requires \\
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 
-// Create Express App Object \\
+// =+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+=
+// Create Express App Object
+// =+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+=
+
 var app = express();
 
-// Application Configuration \\
+// =+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+=
+// Application Configuration
+// =+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+=
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-// Routes \\
+// =+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+=
+// Routes to Magellan's Destinations
+// =+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+=\\
+
+// Home page at Seville
 app.get('/', function(req, res){
-  res.send('Hello')
+  res.sendFile('Seville.html', {root: __dirname + '/public/html/'})
 });
 
-// Creating Server and Listening for Connections \\
+// Canary Islands
+app.get('/tweety', function(req, res){
+  res.sendFile('CanaryIslands.html', {root: __dirname + '/public/html/'})
+});
+
+// Cape Verde
+app.get('/verde', function(req, res){
+  res.sendFile('CapeVerde.html', {root: __dirname + '/public/html/'})
+});
+
+// Strait of Magellan
+app.get('/nohomo', function(req, res){
+  res.sendFile('StraitofMagellan.html', {root: __dirname + '/public/html/'})
+});
+
+// Guam
+app.get('/guacamole', function(req, res){
+  res.sendFile('guam.html', {root: __dirname + '/public/html/'})
+});
+
+// Philippines
+app.get('/pippa', function(req, res){
+  res.sendFile('phillippines.html', {root: __dirname + '/public/html/'})
+});
+
+// =+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+=
+// Creating Server and Listening for Connections
+// =+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+=
 var port = 3000
 app.listen(port, function(){
   console.log('Server running on port ' + port);
-
 })
